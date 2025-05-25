213677dd9ea49ae6bc641f6f48bc73f341e1ef4a
chrisalat <chrisalat@users.noreply.github.com>
2025-05-25 15:05:45 +0000

    deploy: 456305cb23f38bc50eb8051143384b53b1599b97

--- assets/Loading.stories-B8bqrqVY.js
+++
@@ -1,5 +0,0 @@
-import{L as a}from"./Loading-DgjhTzxT.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-BKmLb0-U.js";import"./bem-8m21rGuV.js";const d={title:"prokodo/common/Loading",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{size:{options:["xs","sm","md","lg","xl"]}}},o={args:{size:"md"}};var e,r,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    size: "md"
-  }
-}`,...(s=(r=o.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const c=["Default"];export{o as Default,c as __namedExportsOrder,d as default};
