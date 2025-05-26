ac756ead638e895c867ea26d0d401ce0100e9dde
chrisalat <chrisalat@users.noreply.github.com>
2025-05-26 19:57:57 +0000

    deploy: ecb705bc67a1f7bf20e681755c658c7645bb6082

--- assets/Chip.stories-xyFUb_Tw.js
+++
@@ -1,5 +0,0 @@
-import{C as a}from"./Chip-Cfd4T8q4.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-Bw_7RdAX.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./bem-JthPXG5n.js";const l={title:"prokodo/common/Chip",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{variant:{options:["filled","outlined"],control:{type:"select"}},color:{options:["inherit","default","primary","secondary","error","info","success","warning","white"],control:{type:"select"}}}},e={args:{label:"Chip"}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    label: "Chip"
-  }
-}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,l as default};
