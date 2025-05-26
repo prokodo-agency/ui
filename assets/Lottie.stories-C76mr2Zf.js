5db76cc78ddfa3eeb2606a37f5303a28a8684635
chrisalat <chrisalat@users.noreply.github.com>
2025-05-26 20:13:50 +0000

    deploy: e2f4c607d283382109d52437067c2edab2d7ce09

--- assets/Lottie.stories-C76mr2Zf.js
+++
@@ -1,5 +0,0 @@
-import{L as e}from"./Lottie-B-TnpGmw.js";import"./iframe-BUSOdaE_.js";import"../sb-preview/runtime.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-Bw_7RdAX.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./bem-JthPXG5n.js";const l={title:"prokodo/common/Lottie",component:e,parameters:{layout:"centered"},tags:["autodocs"]},t={args:{animationName:"DataAnalytics"}};var a,o,r;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    animationName: "DataAnalytics"
-  }
-}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,l as default};
