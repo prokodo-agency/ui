60fcabf67c378ab5bd0bfeae913d6275689cb3a7
Christian Salat <salat.christian@googlemail.com>
2025-06-01 16:39:02 +0200

    fix: Resolved lint bug

--- src/components/accordion/Accordion.server.tsx
+++ src/components/accordion/Accordion.server.tsx
@@ -1,5 +1,5 @@
 
-import Animated from "@/components/animated/Temp.server"
+import Animated from "@/components/animated/Animated.server"
 import Button from "@/components/button/Button.server"
 import { Icon } from "@/components/icon"
 
