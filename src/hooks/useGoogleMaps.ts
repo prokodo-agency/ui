517f4a63399c22461aa09c5dcc7982d4db805435
Christian Salat <salat.christian@googlemail.com>
2025-12-06 21:30:39 +0100

    chore: Upgraded all prod dependencies to latest version

--- src/hooks/useGoogleMaps.ts
+++ src/hooks/useGoogleMaps.ts
@@ -1,25 +1,27 @@
-import { Loader } from "@googlemaps/js-api-loader"
+import { importLibrary, setOptions } from "@googlemaps/js-api-loader"
 import { useEffect, useState } from "react"
 
 export const useGoogleMaps = (apiKey: string, reload?: boolean): boolean => {
   const [loaded, setLoaded] = useState(false)
 
   useEffect(() => {
-    const loader = new Loader({
-      apiKey,
-      version: "weekly",
-      libraries: ["maps"],
-    })
+    const loader = async () => {
+      setOptions({
+        key: apiKey,
+        libraries: ["maps"],
+      })
+      await importLibrary("maps")
 
-    loader
-      .importLibrary("marker")
-      .then(() => {
-        setLoaded(true)
-      })
-      .catch((err: ErrorOptions) => {
-        console.error("Error loading Google Maps", err)
-        throw new Error("Error loading Google Map: ", err)
-      })
+      await importLibrary("marker")
+        .then(() => {
+          setLoaded(true)
+        })
+        .catch((err: ErrorOptions) => {
+          console.error("Error loading Google Maps", err)
+          throw new Error("Error loading Google Map: ", err)
+        })
+    }
+    void loader()
   }, [apiKey, reload])
 
   return loaded
